d9b1f2cf2b31b712799ed622d02f482d582d6ad3
Cyber Space <dutymastra@outlook.com>
2021-03-06 23:13:52 +0300

    .

--- DOWNLOAD VIDEO.py
+++ DOWNLOAD VIDEO.py
@@ -1,7 +1,5 @@
 from pytube import YouTube
 link = input('enter the link: ')
 video = YouTube(link)
-stream = video.streams
-stream[1].download(R'D:\videos\Download')
-for i in stream:
-    print(i)
+stream = video.streams.get_highest_resolution()
+stream.download()
